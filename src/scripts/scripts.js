b253e203f7b46d6d2bfd27165f5163a9508bd9e0
tech <tech@sieraj.com>
2023-03-30 20:36:21 +0200

    edit mega menu

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -8,5 +8,36 @@ window.addEventListener("load", () => {
   })
 })
 
+
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+// dark mode 
+function themeMode() {
+  var element = document.getElementById("body");
+  element.classList.toggle("dark-mode");
+}
+
+// mega menu 
+function megaMenu() {
+  var element = document.getElementById("mega-menu");
+  element.classList.toggle("mega-menu--show");
+  var element = document.getElementById("mega-menu-icon");
+  element.classList.toggle("mega-menu-icon--rotate");
+}
+
 const element = document.querySelector('.my-element');
 element.classList.add('animate__animated', 'animate__bounceOutLeft');
